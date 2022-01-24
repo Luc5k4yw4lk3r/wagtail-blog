2a529d10ad8d72087de290dfd821336539472eb8
llc <llc@athento.com>
2022-01-24 11:32:39 -0300

    Scroll vertical made horizonatal

---
+++ static/scripts.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Menu
+    let logoMenu = document.querySelector('.logo-menu');
+    let btnMenuClose = document.querySelector('.btn-menu-close');
+
+    logoMenu.addEventListener("click", (e) => {
+        let containerMenu = document.querySelector('.container-menu');
+        containerMenu.setAttribute('data-visualization', 'true');
+    });
+
+    btnMenuClose.addEventListener("click", (e) => {
+        let containerMenu = document.querySelector('.container-menu');
+        containerMenu.setAttribute('data-visualization', 'false');
+    });
+
+    //Slider
+    const slider = document.querySelector('.slider-inner');
+    const progressBar = document.querySelector('.prog-bar-inner');
+
+    let sliderGrabbed = false;
+
+    slider.parentElement.addEventListener('scroll', (e) => {
+        progressBar.style.width  = `${getScrollPercentage()}%`
+    })
+
+    slider.addEventListener('mousedown', (e) => {
+        sliderGrabbed = true;
+        slider.style.cursor = 'grabbing';
+    })
+
+    slider.addEventListener('mouseup', (e) => {
+        sliderGrabbed = false;
+        slider.style.cursor = 'grab';
+    })
+
+    slider.addEventListener('mouseleave', (e) => {
+        sliderGrabbed = false;
+    })
+
+    slider.addEventListener('mousemove', (e) => {
+        if(sliderGrabbed){
+            slider.parentElement.scrollLeft -= e.movementX;
+        }
+    })
+
+    slider.addEventListener('wheel', (e) =>{
+        e.preventDefault()
+        slider.parentElement.scrollLeft += e.deltaY;
+    })
+
+    function getScrollPercentage(){
+    return ((slider.parentElement.scrollLeft / (slider.parentElement.scrollWidth - slider.parentElement.clientWidth) ) * 100);
+    }
+
+});
